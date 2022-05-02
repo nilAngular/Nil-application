7f36038613e5f47c9652fe47f07051ea42150a68
NileshT <nileshtirpude13@gmail.com>
2022-05-02 20:36:02 +0530

    login issue fixed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder,FormGroup } from '@angular/forms';
+import { FormBuilder,FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginService } from '../login.service';
 
@@ -14,19 +14,23 @@ export class LoginComponent {
 
   constructor(private loginService: LoginService, private router: Router, private formbuilder: FormBuilder) {
     this.loginForm = this.formbuilder.group({
-      username: [],
-      password: [],
+      username: [], 
+      password: []
     });
   }
 
 onFormSubmit(): void {
     const uname = this.loginForm.value.username;
     const pwd = this.loginForm.value.password;
+    console.log(uname);
+    console.log(pwd);
+    
+    
     this.loginService
       .isUserAuthenticated(uname, pwd)
       .subscribe((authenticated) => {
         if (authenticated) {
-          this.router.navigate(['/login']);
+          this.router.navigate(['/books']);
         } else {
           this.invalidCredentialMsg = 'Invalid Credentials. Try again.';
         }
